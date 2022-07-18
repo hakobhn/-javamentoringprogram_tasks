0bb02d1573993eae450f2c3423f62dbb4a055b74
Hakob_Hakobyan3 <hakob.hakobyan.-nd@disney.com>
2022-07-18 13:05:52 +0400

    Initial commit

---
+++ training_module6/src/test/java/com/epam/mentoring/messenger/messenger/MessengerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.epam.mentoring.messenger.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest
+class MessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+		fail();
+	}
+
+}
